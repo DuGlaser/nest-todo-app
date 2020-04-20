15c7a7d0ac709a2ca1e8f73544679d4fbf585b0b
DuGlaser <duglaser0618@gmail.com>
2020-04-20 18:42:09 +0900

    :white_check_mark:ADD the todos.controller's test code

--- src/todos/todos.controller.spec.ts
+++ src/todos/todos.controller.spec.ts
@@ -1,18 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TodosController } from './todos.controller';
+import { TodosService, Todos } from './todos.service';
 
 describe('Todos Controller', () => {
-  let controller: TodosController;
+  let todoController: TodosController;
+  let todoService: TodosService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TodosController],
+      providers: [TodosService],
     }).compile();
 
-    controller = module.get<TodosController>(TodosController);
+    todoController = module.get<TodosController>(TodosController);
+    todoService = module.get<TodosService>(TodosService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should to create todo', async () => {
+    const todo: Todos = {
+      body: 'test',
+      isDone: false,
+    };
+    jest.spyOn(todoService, 'createTodo').mockImplementation(() => [todo]);
+
+    expect(await todoController.createNewTodo(todo)).toBe(todo);
+  });
+
+  it('should return todo array', async () => {
+    const todo: Todos = {
+      body: 'test',
+      isDone: false,
+    };
+    jest.spyOn(todoService, 'getTodos').mockImplementation(() => [todo]);
+    expect(await todoController.getAll()).toStrictEqual([todo]);
   });
 });
