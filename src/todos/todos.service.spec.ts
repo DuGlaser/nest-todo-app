9c86945dcccf02bf78c6c50e6832afa6ee60b6d3
DuGlaser <duglaser0618@gmail.com>
2020-04-20 21:12:40 +0900

    :white_check_mark:ADD the editTodos func's test code

--- src/todos/todos.service.spec.ts
+++ src/todos/todos.service.spec.ts
@@ -12,12 +12,27 @@ describe('TodosService', () => {
     service = module.get<TodosService>(TodosService);
   });
 
-  it('should be defined', async () => {
+  it('should return todo array', async () => {
     const todo: Todos = {
       body: 'test',
       isDone: false,
     };
     service.createTodo(todo);
     expect(service.getTodos()).toStrictEqual([todo]);
+  });
+
+  it('should edit todo array', async () => {
+    const todo1: Todos = {
+      body: 'test',
+      isDone: false,
+    };
+
+    const todo2: Todos = {
+      body: 'edit',
+      isDone: false,
+    };
+    service.createTodo(todo1);
+    service.editTodo(todo2, '0');
+    expect(service.getTodos()).toStrictEqual([todo2]);
   });
 });
