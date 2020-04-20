bd191598f0b3be8e536733814a884a4de2f37453
DuGlaser <duglaser0618@gmail.com>
2020-04-20 18:14:35 +0900

    :sparkles:ADD the createTodo & getTodos func

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Post, Get } from '@nestjs/common';
+import { TodosService, Todos } from './todos.service';
+import { TodoDto } from './todos.dto';
 
 @Controller('todos')
-export class TodosController {}
+export class TodosController {
+  constructor(private readonly todoService: TodosService) {}
+
+  @Post()
+  async createNewTodo(@Body() todo: TodoDto) {
+    this.todoService.createTodo(todo);
+    return todo;
+  }
+
+  @Get()
+  getAll(): Todos[] {
+    return this.todoService.getTodos();
+  }
+}
--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
 
+export interface Todos {
+  body: string;
+  isDone: boolean;
+}
+
 @Injectable()
-export class TodosService {}
+export class TodosService {
+  private readonly todos: Todos[] = [];
+
+  getTodos(): Todos[] {
+    return this.todos;
+  }
+
+  // setTodos(todo: Todos): Promise<any> {
+  //   return new Promise(resolve => {
+  //     this.todos.push(todo);
+  //     resolve(todo);
+  //   });
+  // }
+
+  createTodo(todo: Todos) {
+    this.todos.push(todo);
+  }
+}
