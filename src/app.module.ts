7169c0475c5a28e460b2ca094216b1fae33408df
DuGlaser <duglaser0618@gmail.com>
2020-04-22 13:10:08 +0900

    :sparkles:ADD the typeORM init setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosController } from './todos/todos.controller';
 import { TodosService } from './todos/todos.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'develop',
+      password: 'password',
+      database: 'develop',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController, TodosController],
   providers: [AppService, TodosService],
 })
