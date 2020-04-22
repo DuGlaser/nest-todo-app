4e9de0c8c69175ddabd6303b102e06ae1d7a48bc
DuGlaser <duglaser0618@gmail.com>
2020-04-22 13:30:30 +0900

    :recycle:FIX detabase setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,19 +4,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosController } from './todos/todos.controller';
 import { TodosService } from './todos/todos.service';
+import { Todo } from './todos/todos.entity';
+import { TodosModule } from './todos/todos.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+      host: 'db-server',
       port: 3306,
       username: 'develop',
       password: 'password',
       database: 'develop',
-      entities: [],
+      entities: [Todo],
       synchronize: true,
     }),
+    TodosModule,
   ],
   controllers: [AppController, TodosController],
   providers: [AppService, TodosService],
